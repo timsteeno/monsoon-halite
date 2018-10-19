7a93647b2697545b56ab5d3cef69121e7ccb2d8d
timsteeno <theflamingoking@gmail.com>
2018-10-19 16:58:20 -0500

    Created basic structure from example bot. Added get_command function to make ship move decision and return.

---
+++ Monsoon.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Python 3.6
+
+import hlt
+from hlt import constants
+from hlt.positionals import Direction
+import random
+import logging
+
+
+def get_command(game_map, ship):
+    if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
+        return ship.move(random.choice([Direction.North, Direction.South, Direction.East, Direction.West]))
+    else:
+        return ship.stay_still()
+
+
+""" <<<Game Begin>>> """
+
+# This game object contains the initial game state.
+game = hlt.Game()
+# At this point "game" variable is populated with initial map data.
+# This is a good place to do computationally expensive start-up pre-processing.
+# As soon as you call "ready" function below, the 2 second per turn timer will start.
+game.ready("Monsoon (Potato)")
+
+# Now that your bot is initialized, save a message to yourself in the log file with some important information.
+#   Here, you log here your id, which you can always fetch from the game object by using my_id.
+logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
+
+""" <<<Game Loop>>> """
+
+while True:
+    # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
+    #   running update_frame().
+    game.update_frame()
+    # You extract player metadata and the updated map metadata here for convenience.
+    me = game.me
+    game_map = game.game_map
+
+    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
+    #   end of the turn.
+    command_queue = []
+
+    # Add a move for each ship
+    for ship in me.get_ships():
+        command_queue.append(get_command(game_map, ship))
+
+    # Decide whether to spawn a new ship
+    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+        command_queue.append(me.shipyard.spawn())
+
+    # Send your moves back to the game environment, ending this turn.
+    game.end_turn(command_queue)
