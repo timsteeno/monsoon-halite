ca5666115c11f17b4b8c129c6d51b38621f4a39a
timsteeno <theflamingoking@gmail.com>
2018-10-20 11:38:45 -0500

    Added some start to invading opponents shipyards.

--- Monsoon.py
+++ Monsoon.py
@@ -14,8 +14,10 @@ shipyard_position = game.me.shipyard.position
 
 # Configure the dict for which bots are headed to shipyard
 is_depositing = {}
-for ship in game.me.get_ships():
-    is_depositing[ship.id] = False
+
+# Configure the dict for which bots are headed to enemy shipyards
+is_invading = {}
+targets = {}
 
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
 game.ready("Monsoon (Potato)")
@@ -34,6 +36,11 @@ while True:
     me = game.me
     game_map = game.game_map
 
+    if game.turn_number == 1:
+        structure_list = game_info.get_structure_list(game_map)
+        target_list = [position for position in structure_list if position != shipyard_position]
+        logging.info('structure target list: {}'.format(target_list))
+
     # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
     #   end of the turn.
     command_queue = []
@@ -44,13 +51,32 @@ while True:
             # We made the deposit. Turn off is_depositing and get a new command
             is_depositing[ship.id] = False
 
+            # check here if it's time to send this one on kamikaze to a base
+            if len(me.get_ships()) >= 12 and len(target_list) > 0:
+                logging.info("num_ships: {}".format(len(me.get_ships())))
+                logging.info("target_list: {}".format(target_list))
+                target = target_list.pop(0)
+                logging.info("target: {}".format(target))
+                logging.info("ship id: {}".format(ship.id))
+
+                is_invading[ship.id] = True
+                targets[ship.id] = target
+
         if ship.halite_amount >= constants.MAX_HALITE*.98:
             # We're full. Set is_depositing so we'll head back.
             is_depositing[ship.id] = True
 
+        try:
+            is_invading[ship.id]
+        except KeyError:
+            is_invading[ship.id] = False
+
         if is_depositing[ship.id]:
             # We are depositing but still aren't at the shipyard. Head there.
             command_queue.append(ship.move(game_map.naive_navigate(ship, shipyard_position)))
+        elif is_invading[ship.id]:
+            # We are headed towards targets[ship.id] to invade
+            command_queue.append(ship.move(game_map.naive_navigate(ship, targets[ship.id])))
         else:
             command_queue.append(game_info.get_command(game_map, ship))
 
--- game_info.py
+++ game_info.py
@@ -1,14 +1,31 @@
-from hlt.positionals import Direction
+from hlt.positionals import Direction, Position
 from hlt import constants
 import logging
 
 
 def get_future_ev(current_halite, max_halite):
+    """Given current_halite: the halite at this position, and max_halite: the largest neighboring halite amount,
+    return the expected value if the ship stays still for two turns vs moving to the neighbor position.
+    Used by ships in get_command to determine a positive EV move over the next two turns."""
     two_turn_stay_ev = current_halite * 1 / constants.EXTRACT_RATIO
     two_turn_stay_ev += current_halite * (1 - 1 / constants.EXTRACT_RATIO) * (1 / constants.EXTRACT_RATIO)
     two_turn_move_ev = max_halite * 1 / constants.EXTRACT_RATIO
     two_turn_move_ev -= current_halite * 1 / constants.MOVE_COST_RATIO
     return two_turn_stay_ev, two_turn_move_ev
+
+
+def get_structure_list(game_map):
+    """Given a game map, return a numpy array object representing whether each cell is a dropoff or shipyard."""
+    width = game_map.width
+    height = game_map.height
+    structure_list = []
+
+    for x in range(height):
+        for y in range(width):
+            structure = game_map[Position(x, y)].has_structure
+            if structure:
+                structure_list.append(Position(x,y))
+    return structure_list
 
 
 def get_command(game_map, ship):
@@ -32,23 +49,23 @@ def get_command(game_map, ship):
         # For now we'll crash to the right to see if we can free up the situation
         best_neighbor_direction = Direction.East
 
-    logging.info('Max halite nearby: {} direction: {}'.format(
-        max_halite, Direction.convert(best_neighbor_direction)))
-    logging.info('Halite storage is at {}'.format(ship.halite_amount))
-    logging.info('Current halite at this position: {}'.format(current_halite))
+#    logging.info('Max halite nearby: {} direction: {}'.format(
+#        max_halite, Direction.convert(best_neighbor_direction)))
+#    logging.info('Halite storage is at {}'.format(ship.halite_amount))
+#    logging.info('Current halite at this position: {}'.format(current_halite))
 
     two_turn_stay_ev, two_turn_move_ev = get_future_ev(current_halite, max_halite)
 
     if two_turn_stay_ev > two_turn_move_ev:
         # Better to stay
-        logging.info("I should stay here. EV={} vs {}".format(
-            current_halite * .25 + current_halite * (.75 * .25),
-            max_halite * .25 - current_halite * .1
-        ))
+#        logging.info("I should stay here. EV={} vs {}".format(
+#            current_halite * .25 + current_halite * (.75 * .25),
+#            max_halite * .25 - current_halite * .1
+#        ))
         return ship.stay_still()
     else:
-        logging.info("I should move. EV={} vs {}".format(
-            current_halite * .25 + current_halite * (.75 * .25),
-            max_halite * .25 - current_halite * .1
-        ))
+#        logging.info("I should move. EV={} vs {}".format(
+#            current_halite * .25 + current_halite * (.75 * .25),
+#            max_halite * .25 - current_halite * .1
+#        ))
         return ship.move(best_neighbor_direction)
